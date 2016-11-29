dac3917f74de71d435d30ba7796b10b20ae33751
Mitch <mitch@localhost.localdomain>
2016-11-28 22:58:28 -0500

    Added Argv tags
    Added '-' tags for changing settings
    
    Also added '-h' for a help menu of all the settings to change
    and how to use the program from the terminal

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ class SineWave:
         ampl = pow(2,8*self.bitDepth) - 1
         ampl = (ampl / 2) * self.volume
 
-        for i in range(self.length * self.samples):
+        for i in range(int(self.length * self.samples)):
             z = i / y
             a = math.sin(z*math.pi)
             b = a * ampl
@@ -83,13 +83,63 @@ def printArray(arr):
     for i in arr:
         print(i)
 
+def getArgs():
+    args = sys.argv
+
+    freq = 500
+    samples = 44100
+    bitdepth = 24
+    volume = 1
+    length = 1
+    channels = 1
+    filename = "500hzSineWave.wav"
+
+    for i in range(len(args)):
+        a = args[i]
+        if a == "-f":
+            freq = float(args[i+1])
+        elif a == "-s":
+            samples = float(args[i+1])
+        elif a == "-b":
+            bitdepth = float(args[i+1])
+        elif a == "-v":
+            volume = float(args[i+1])
+        elif a == "-l":
+            length = float(args[i+1])
+        elif a == "-c":
+            channels = int(args[i+1])
+        elif a == "-n":
+            filename = str(args[i+1])
+        elif a == "-h":
+            print(""" 
+Arg   Description                Default Value
+-----------------------------------------------------------
+-f  : frequency                 : 500
+-s  : samples per second        : 44100
+-b  : bit depth                 : 24
+-v  : volume from 0.0 to 1.0    : 1.0
+-l  : length in seconds 0.0     : 1.0
+-c  : channels                  : 1  (2 not supported)
+-n  : filename                  : 500hzSineWave.wav
+-h  : help
+
+Example:
+-f 500 -s 44100 -b 24 -v 0.5 -l 1 -c 1
+-----------------------------------------------------------
+            """)
+            return 0
+        
+    return (freq,samples,bitdepth,volume,length,channels,filename)
+
 def main():
-    wav = SineWave(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),
-                   float(sys.argv[4]),int(sys.argv[5]))
-    # wav = SineWave(100,44100,24,1,2)
-    f = WaveFile("sinewave.wav",1,wav.bitDepth,wav.samples)
-    f.addAudio(wav.getBytes())
-    f.saveAudio("/home/mitch/Desktop/")
+    vals = getArgs()
+
+    if vals != 0:
+        wav = SineWave(vals[0],vals[1],vals[2],vals[3],vals[4])
+        # wav = SineWave(100,44100,24,1,2)
+        f = WaveFile(vals[6],vals[5],wav.bitDepth,wav.samples)
+        f.addAudio(wav.getBytes())
+        f.saveAudio("")
 
 
 
