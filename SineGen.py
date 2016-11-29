1da461fe0ccafe70460b4cebdacf8739db65f5f0
Mitch <mitch@localhost.localdomain>
2016-11-28 23:06:12 -0500

    Modified help Prevented program from creating wav file with no terminal args removed workinstuff.py

--- SineGen.py
+++ SineGen.py
@@ -86,13 +86,16 @@ def printArray(arr):
 def getArgs():
     args = sys.argv
 
+    if len(args) == 1:
+        return 0
+
     freq = 500
     samples = 44100
     bitdepth = 24
     volume = 1
     length = 1
     channels = 1
-    filename = "500hzSineWave.wav"
+    filename = "DefaultSineWave.wav"
 
     for i in range(len(args)):
         a = args[i]
@@ -110,18 +113,18 @@ def getArgs():
             channels = int(args[i+1])
         elif a == "-n":
             filename = str(args[i+1])
-        elif a == "-h":
+        elif a == "-h" or a == "--help":
             print(""" 
-Arg   Description                Default Value
+Arg           Description                Default Value
 -----------------------------------------------------------
--f  : frequency                 : 500
--s  : samples per second        : 44100
--b  : bit depth                 : 24
--v  : volume from 0.0 to 1.0    : 1.0
--l  : length in seconds 0.0     : 1.0
--c  : channels                  : 1  (2 not supported)
--n  : filename                  : 500hzSineWave.wav
--h  : help
+-f          : frequency                 : 500
+-s          : samples per second        : 44100
+-b          : bit depth                 : 24
+-v          : volume from 0.0 to 1.0    : 1.0
+-l          : length in seconds 0.0     : 1.0
+-c          : channels                  : 1  (2 not supported)
+-n          : filename                  : DefaultSineWave.wav
+-h, --help  : help
 
 Example:
 -f 500 -s 44100 -b 24 -v 0.5 -l 1 -c 1
